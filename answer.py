8d79b655c0917395dfc4cd06a32a7bea2d36a13c
Ezequiel Sanchez <squiel91@gmail.com>
2016-12-05 15:02:52 -0300

    With fact checker

--- answer.py
+++ answer.py
@@ -1,6 +1,6 @@
 from pdb import set_trace as st
 
-q = [
+qw = [
 	('for who', [':ARG2']),
 	('how long', [':duration']),
 	('from where', [':origin']),
@@ -26,27 +26,44 @@ class Answer:
 
 
 	def answer(self, question_AMR_graph, inquire):
-		relations_inquired, word = relation_to_look_for(inquire, q)
-		if not relations_inquired:
-			raise Exception("Please ask a question including at least one of this words: " + ', '.join([q_tuple[0] for q_tuple in q]))
+		relationship = relation_to_look_for(inquire, qw)
+		check_fact = False
+		relations_inquired, word = (None, None)
+		if relationship:
+			relations_inquired, word = relation_to_look_for(inquire, qw)
+		else:
+			check_fact = True
 		print("Answer to '{}':\n".format(inquire))
 		print("AMR representation:")
 		question_AMR_graph.show()
 
 		print("\n")
+		if check_fact:
+			word = "fact"
 		print('{}:'.format(word))
 		answers_quantity = 0
 		phrase_index = 0
 		for corpus_sent in self.corpus:
 			phrase_index += 1
 			if corpus_sent.partial_match(question_AMR_graph):
-				for relation in relations_inquired:
-					answer_tree = corpus_sent.adjacents(question_AMR_graph.root, label=relation)
-					corpus_sent.change_root(answer_tree)
-					if answer_tree:
-						print("----------------")
-						corpus_sent.show() 
-						print("{}: {}".format(phrase_index, self.originals[phrase_index - 1]))
-						answers_quantity += 1
+				if check_fact:
+					print("----------------")
+					corpus_sent.show() 
+					print("{}: {}".format(phrase_index, self.originals[phrase_index - 1]))
+					answers_quantity += 1
+				else:
+					for relation in relations_inquired:
+						answer_tree = corpus_sent.adjacents(question_AMR_graph.root, label=relation)
+						corpus_sent.change_root(answer_tree)
+						if answer_tree:
+							print("----------------")
+							corpus_sent.show() 
+							print("{}: {}".format(phrase_index, self.originals[phrase_index - 1]))
+							answers_quantity += 1
 						
 		print("\n{} answer(s)".format(answers_quantity))
+		if check_fact:
+			if answers_quantity > 0:
+				print("Is true")
+			else:
+				print("No evidence")
